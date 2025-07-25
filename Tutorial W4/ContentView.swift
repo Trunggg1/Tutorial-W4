9628c27ac31b0d85bb5d298b28b3c7c3e2a0d1a5
Trunggg1 <quoctrung090803@gmail.com>
2025-07-25 11:19:10 +0700

    Initial Commit

---
+++ Tutorial W4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tutorial W4
+//
+//  Created by Trung Nguyen Quoc on 25/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tutorial W4/Tutorial_W4App.swift
@@ -0,0 +1,17 @@
+//
+//  Tutorial_W4App.swift
+//  Tutorial W4
+//
+//  Created by Trung Nguyen Quoc on 25/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Tutorial_W4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
